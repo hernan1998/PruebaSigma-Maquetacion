cd79b96ec18ad9c5dc024431df372a8a6b0c3a8e
Hernan Parra <hernandre1122@gmail.com>
2020-08-07 15:46:32 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JsonService } from './json.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prueba-sigma';
+
+  // ----------- Variables -----------------------------
+  Departamentos = [];
+
+  // ------------ Code ---------------------------------
+  constructor(public json: JsonService) { }
+
+  ngOnInit() {
+    this.json.getJson('http://sigma-studios.s3-us-west-2.amazonaws.com/test/colombia.json').subscribe((data: any) => {
+      data.forEach(element => {
+        this.Departamentos.push(element);
+      });
+      console.log(this.Departamentos);
+    });
+  }
 }
